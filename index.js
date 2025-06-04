e1d90b0a215738530c726bd990ce0a473c7cef47
astik0398 <kumarastik0398@gmail.com>
2025-06-04 10:01:52 +0530

    added console and fixes for started_date

--- index.js
+++ index.js
@@ -64,15 +64,14 @@ const getFormattedDate = () => {
 };
 
 const getCurrentDate = () => {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, "0");
-  const day = String(now.getDate()).padStart(2, "0");
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const now = moment().tz("Asia/Kolkata"); // Explicitly set to Asia/Kolkata
+  const year = now.get("year");
+  const month = String(now.get("month") + 1).padStart(2, "0");
+  const day = String(now.get("date")).padStart(2, "0");
+  const hours = String(now.get("hour")).padStart(2, "0");
+  const minutes = String(now.get("minute")).padStart(2, "0");
 
   console.log(`${year}-${month}-${day} ${hours}:${minutes}`);
-
   return `${year}-${month}-${day} ${hours}:${minutes}`;
 };
 
