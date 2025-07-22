40775317770b9d89c48400837e23787e155d2af6
astik0398 <kumarastik0398@gmail.com>
2025-07-22 15:33:37 +0530

    tweaked the prompt to handle words like tonigh01

--- index.js
+++ index.js
@@ -3886,5 +3886,5 @@ app.post("/update-reminder", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
   makeTwilioRequest();
-  // initializeReminders();
+  initializeReminders();
 });
