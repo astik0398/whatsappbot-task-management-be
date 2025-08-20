de2201f086a875fe1f5a00b7b7e20ed82ce1a883
astik0398 <kumarastik0398@gmail.com>
2025-08-20 19:30:51 +0530

    added 9am trigger 1

--- index.js
+++ index.js
@@ -4204,5 +4204,5 @@ app.get("/show-task-summary", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
   makeTwilioRequest();
-  // initializeReminders();
+  initializeReminders();
 });
